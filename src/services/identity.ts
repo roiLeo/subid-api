16c3b35b56903cc8f8ac8ad63d464b647560fb27
VVhatIsLove <sam4uk.v1ad@gmail.com>
2023-02-16 13:51:20 +0200

    Fetch Subsocial identity from subsocial squid

--- src/services/identity.ts
+++ src/services/identity.ts
@@ -1,12 +1,16 @@
-import { resolveSubsocialApi } from '../connections'
-import { WithApis } from './types'
-import { runQueryOrUndefined, toGenericAccountId } from './utils';
+import { WithApis, SubsocialProfilesResult } from './types';
+import { runQueryOrUndefined, toGenericAccountId } from './utils'
 import { Registration } from '@polkadot/types/interfaces'
 import { hexToString } from '@polkadot/util'
 import { ApiPromise } from '../connections/networks/types'
-import { Option } from '@polkadot/types';
-import { isEmptyArray, isEmptyObj } from '@subsocial/utils';
+import { Option } from '@polkadot/types'
+import { isEmptyArray, isEmptyObj } from '@subsocial/utils'
 import { pick } from 'lodash'
+import { GraphQLClient, gql } from 'graphql-request'
+import { SUBSOCIAL_GRAPHQL_CLIENT } from '../constant/index'
+import { encodeAddress } from '@polkadot/util-crypto'
+
+const subsocialGraphQlClient = new GraphQLClient(SUBSOCIAL_GRAPHQL_CLIENT)
 
 const identitiesInfo = {}
 
@@ -46,10 +50,10 @@ type GetIdentitiesProps = WithApis & {
 }
 
 const parseIdentity = (
-  chain: string, 
-  accounts: string[], 
-  accountsWithSubIdentity: string[], 
-  superOfMultiObj: Record<string, any>, 
+  chain: string,
+  accounts: string[],
+  accountsWithSubIdentity: string[],
+  superOfMultiObj: Record<string, any>,
   identities?: Option<any>[]
 ) => {
   if (!identities) return undefined
@@ -60,14 +64,14 @@ const parseIdentity = (
     const identity = identityOpt.unwrapOr(undefined) as Registration | undefined
 
     if (!identity) return undefined
-    
+
     const account = accountsWithSubIdentity[i]
-    
+
     const {
       info: { additional, pgpFingerprint, ...fields },
       judgements
     } = identity.toJSON() as any
-  
+
     const isVerify = !!judgements.filter((x) => x[1].isReasonable).length
 
     const value = {
@@ -82,12 +86,12 @@ const parseIdentity = (
     parsedIdentities[account] = value
   })
 
-  accounts.forEach((account)=> {
+  accounts.forEach((account) => {
     const genericAccountId = toGenericAccountId(account)
 
     const identity = parsedIdentities[toGenericAccountId(genericAccountId)] || {}
 
-    if(!isEmptyObj(identity)) {
+    if (!isEmptyObj(identity)) {
       identitiesInfo[chain][genericAccountId] = identity
       return
     }
@@ -95,12 +99,12 @@ const parseIdentity = (
     const superOf = superOfMultiObj[genericAccountId] || {}
 
     const parent = superOf?.parent
-    
+
     const genericAccount = parent ? toGenericAccountId(parent) : ''
-    
+
     const parentIdentity = parsedIdentities[genericAccount] || {}
-    
-    if(isEmptyObj(parentIdentity)) return
+
+    if (isEmptyObj(parentIdentity)) return
 
     identitiesInfo[chain][genericAccountId] = {
       info: {
@@ -114,7 +118,9 @@ const getIdentity = async (api: ApiPromise, accounts: string[], chain: string) =
   const forceUpdate = needUpdate && needUpdate()
   const cacheDataByChain = identitiesInfo?.[chain]
 
-  const needFetch = cacheDataByChain ? accounts.filter(account => !Object.keys(cacheDataByChain).includes(account)) : []
+  const needFetch = cacheDataByChain
+    ? accounts.filter((account) => !Object.keys(cacheDataByChain).includes(account))
+    : []
 
   if (!cacheDataByChain) {
     identitiesInfo[chain] = {}
@@ -122,7 +128,7 @@ const getIdentity = async (api: ApiPromise, accounts: string[], chain: string) =
 
   if (!isEmptyArray(needFetch)) {
     const accountsToFetch = forceUpdate ? accounts : needFetch
-    const superOfMulti = await api.query.identity.superOf.multi(accountsToFetch) as Option<any>[]
+    const superOfMulti = (await api.query.identity.superOf.multi(accountsToFetch)) as Option<any>[]
 
     const superOfMultiObj = {}
     const parentIds = []
@@ -130,9 +136,9 @@ const getIdentity = async (api: ApiPromise, accounts: string[], chain: string) =
     accountsToFetch.forEach((account, i) => {
       const superOf = superOfMulti[i].unwrapOr(undefined)
 
-      if(!superOf) return
+      if (!superOf) return
 
-      const [ key, value ] = superOf
+      const [key, value] = superOf
 
       const parentId = key.toHuman()
 
@@ -146,8 +152,10 @@ const getIdentity = async (api: ApiPromise, accounts: string[], chain: string) =
 
     const accountsWithSubIdentity = [...accountsToFetch, ...parentIds]
 
-    const identities = (await api.query.identity.identityOf.multi(accountsWithSubIdentity)) as Option<any>[]
-    parseIdentity(chain, accountsToFetch, accountsWithSubIdentity, superOfMultiObj, identities)    
+    const identities = (await api.query.identity.identityOf.multi(
+      accountsWithSubIdentity
+    )) as Option<any>[]
+    parseIdentity(chain, accountsToFetch, accountsWithSubIdentity, superOfMultiObj, identities)
   }
 
   const result = pick(identitiesInfo[chain], accounts)
@@ -155,37 +163,74 @@ const getIdentity = async (api: ApiPromise, accounts: string[], chain: string) =
   return result
 }
 
+export const GET_SUBSOCIAL_PROFILES = gql`
+  query GetProfilesData($ids: [String!]) {
+    accounts(where: { id_in: $ids }) {
+      profileSpace {
+        content
+        createdAtBlock
+        createdAtTime
+        createdByAccount {
+          id
+        }
+        email
+        name
+        linksOriginal
+        hidden
+        id
+        updatedAtTime
+        postsCount
+        image
+        tagsOriginal
+        summary
+        about
+        ownedByAccount {
+          id
+        }
+        experimental
+      }
+    }
+  }
+`
+
+const getSubsococilaIdentity = async (accounts: string[]) => {
+  const subsocialIdentities = {}
+
+  const encodedAccounts = accounts.map(account => encodeAddress(account, 28))
+  const spaces: SubsocialProfilesResult = 
+    await subsocialGraphQlClient.request(GET_SUBSOCIAL_PROFILES, { ids: encodedAccounts })
+
+  spaces.accounts.forEach((space) => {
+    const profileSpace = space.profileSpace
+
+    if(profileSpace) {
+      subsocialIdentities[toGenericAccountId(profileSpace.ownedByAccount.id)] = profileSpace
+    }
+  })
+
+  return subsocialIdentities
+}
+
 export const getIdentities = async ({
-  apis: { kusama, polkadot, shiden, subsocial },
+  apis: { kusama, polkadot, shiden },
   accounts
 }: GetIdentitiesProps) => {
   const identities = {}
 
-  const subsocialApi = resolveSubsocialApi(subsocial)
 
   const [kusamaIdentity, polkadotIdentity, shidenIdentity, subsocialIdentity] = await Promise.all([
     runQueryOrUndefined(kusama, async (api) => getIdentity(api, accounts, 'kusama')),
     runQueryOrUndefined(polkadot, async (api) => getIdentity(api, accounts, 'polkadot')),
     runQueryOrUndefined(shiden, async (api) => getIdentity(api, accounts, 'shiden')),
-    runQueryOrUndefined(subsocialApi, async (api) => {
-      const subsocialIdentities = {}
-
-      const spaces = await api.findProfileSpaces(accounts)
-
-      spaces.forEach((space) => {
-        subsocialIdentities[toGenericAccountId(space.struct.ownerId)] = space
-      })
-      
-      return subsocialIdentities
-    })
+    getSubsococilaIdentity(accounts)
   ])
 
-  accounts.forEach(account => {
+  accounts.forEach((account) => {
     identities[account] = {
       kusama: kusamaIdentity?.[account],
       polkadot: polkadotIdentity?.[account],
       shiden: shidenIdentity?.[account],
-      subsocial: subsocialIdentity?.[account],
+      subsocial: subsocialIdentity?.[account]
     }
   })
 
--- src/services/types.ts
+++ src/services/types.ts
@@ -24,3 +24,27 @@ export type NetworksData = Record<SupportedNetworks, any>
 export type GetDataByAccountProps = WithApis & {
   account: string
 }
+
+export type SubsocialProfile = {
+  content: string
+  createdAtBlock: string
+  createdAtTime: string
+  createdByAccount: Record<'id', string>
+  email: string
+  name: string
+  linksOriginal: string
+  hidden: string
+  id: string
+  updatedAtTime: string
+  postsCount: number
+  image: string
+  tagsOriginal: string
+  summary: string
+  about: string
+  ownedByAccount: Record<'id', string>
+  experimental: Record<string, string>
+}
+
+export type SubsocialProfilesResult = {
+  accounts: Record<'profileSpace', SubsocialProfile>[]
+}
