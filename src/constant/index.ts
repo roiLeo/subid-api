5903d22172d512d4283083a693e0ae391ff58e6a
Oleh Mell <oleh.melnechyk@gmail.com>
2023-04-06 01:09:19 +0300

    Update onFinality key

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -14,7 +14,7 @@ export const validatorsStakingNetworks = [
 ]
 
 export const VALIDATOR_STAKING_REWARDS_API_KEY = 'f00511ab0c5e422796b8f5d9c7029fbe'
-export const ONFINALITY_API_KEY = '7f3dc170-944b-4830-9e62-863be28ac644'
+export const ONFINALITY_API_KEY = '8e789deb-3ab1-4c39-bd67-8ff4b93ba0e0'
 
 export const SUBSOCIAL_GRAPHQL_CLIENT = 'https://squid.subsquid.io/subsocial/graphql'
 
