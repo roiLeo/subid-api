be3fa44f4aa28ebce84fe183cff0b6c03c754443
Sam <sam4uk.v1ad@gmail.com>
2023-04-14 15:26:21 +0300

    Change onfinality api key

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -14,7 +14,7 @@ export const validatorsStakingNetworks = [
 ]
 
 export const VALIDATOR_STAKING_REWARDS_API_KEY = 'f00511ab0c5e422796b8f5d9c7029fbe'
-export const ONFINALITY_API_KEY = '28209e0e-fc76-481e-a80f-745ffdf477d7'
+export const ONFINALITY_API_KEY = '9be88d8b-7470-40b2-8a09-1945b00d5844'
 
 // export const ONFINALITY_API_KEY = '75c71c0d-8fc2-4a6b-b7bb-7ed742293513' // test api key for localhost
 
